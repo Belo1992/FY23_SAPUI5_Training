233e7e2729c61d313f6a82c484599a468cbcefe0
Isabelo Jr <isabelo.m.lagria.jr@accenture.com>
2025-06-02 04:45:12 +0000

    clear button update

--- controller/MainView.controller.js
+++ controller/MainView.controller.js
@@ -5,6 +5,20 @@ sap.ui.define([
 
     return Controller.extend("project1.controller.MainView", {
         onInit() {
+        },
+        onClear: function () {
+            // Get the View
+            let oView = this.getView();
+        
+            // Clear Input fields
+            oView.byId("inptName").setValue("");
+            oView.byId("inptStreet").setValue("");
+            oView.byId("inptHouse").setValue("");
+            oView.byId("inptZip").setValue("");
+            oView.byId("lblCity").setValue("");
+        
+            // Reset the selected key of the Select control
+            oView.byId("country").setSelectedKey("");
         }
     });
 });
